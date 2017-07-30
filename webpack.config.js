4b9bdb1915e389e50d61ff92d664d4f4d42d39e7
arotter <andreas.rotter.kontakt@gmail.com>
2017-07-30 13:08:43 +0200

    adjust webpack built output path

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     './src/client.js'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'public/js'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
