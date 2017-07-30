af0fee27cb8c3c2409518e284698a1d3bd16a4eb
arotter <andreas.rotter.kontakt@gmail.com>
2017-07-30 09:00:23 +0200

    add immutable Set, refactor css

--- src/components/file-upload-input.jsx
+++ src/components/file-upload-input.jsx
@@ -2,34 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {observer} from 'mobx-react'
 import Dropzone from 'react-dropzone'
+import { Set } from 'immutable'
 
 export default class FileUploadInput extends React.Component {
   constructor() {
     super()
-    this.state = { files: [] }
+    this.state = { files: Set() }
+  }
+
+  filterMimeTypes(fileList) {
+    const filteredFiles = filesList.filter( f => {
+      return true
+    })
+    return Set(filteredFiles)
   }
 
   onDrop(files) {
+    //const newFiles = this.filterMimeTypes(files)
+    const newFiles = Set(files)
     this.setState((prevState, props) => {
-      return { files: prevState.files.concat(files)}
+      return {
+        files: prevState.files.merge(newFiles)
+      }
     })
   }
 
   render() {
     return (
       <section>
-        <div>
-          <Dropzone onDrop={this.onDrop.bind(this)}>
-            <p>Drag'n'Drop der zu importierenden Dateien hierauf oder einfach klicken und Dateien auswählen.</p>
-          </Dropzone>
-        </div>
-        <div>
-          <h2>Dateien</h2>
-          <ul style={{listStyleType: 'none'}}>
-            {
-              this.state.files.map(f => <li key={f.name}>{f.name} </li>)
-            }
-          </ul>
+        <div className="fileUpload--container">
+          <div className="fileUpload--dropzone">
+            <Dropzone onDrop={this.onDrop.bind(this)} className="dropzone-container">
+              <p>Drag'n'Drop der zu importierenden Dateien hierauf oder einfach klicken und Dateien auswählen.</p>
+            </Dropzone>
+          </div>
+          <div className="fileUpload--filelist">
+            <h2>Dateien</h2>
+            <ul>
+              {
+                this.state.files.map(f => <li key={f.name}>{f.name} </li>)
+              }
+            </ul>
+          </div>
         </div>
       </section>
     );
