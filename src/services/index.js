3deb47116c86c6def4d1175b8618140d1c7c9dae
arotter <andreas.rotter.kontakt@gmail.com>
2017-08-08 09:43:43 +0200

    use upload service

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,9 @@
+const civicrmRestApi = require('./civicrmrestapi/civicrmrestapi.service.js');
+const importApi = require('./import/import.service.js')
+const restApiWorker = require('./rest-api-worker/rest-api-worker.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(civicrmRestApi);
+  app.configure(importApi);
+  app.configure(restApiWorker);
 };
