e0c0a1adc60ead1140946b7899bddb6d377c4990
arotter <andreas.rotter.kontakt@gmail.com>
2017-09-05 16:34:10 +0200

    add newsletter combining script

---
+++ src/constants.js
@@ -0,0 +1,55 @@
+const ID = 'ID'
+const GENDER = 'Herr/Frau'
+const TITLE = 'Titel'
+const FIRST = 'Vorname'
+const LAST = 'Nachname'
+const ORGA = 'Institution'
+const FUNCTION = 'Funktion'
+const OPT_OUT = 'Kontakt erwünscht?'
+const EMAIL_WORK = 'Email beruflich'
+const EMAIL_HOME = 'Email privat'
+const DONATE_PLATTFORM = 'Spendenplattform'
+const PHONE_WORK = 'Telefon berufl.'
+const MOBILE_HOME = 'Mobilnummer'
+const PHONE_HOME = 'Telefon privat'
+const POSTCODE = 'Adresse - PLZ'
+const CITY = 'Adresse - Ort'
+const STREET = 'Adresse - Straße'
+const COUNTRY = 'Adresse - Land'
+const BIRTH = 'Geburtsdatum'
+const GROUP = 'Gruppe'
+const TAG = 'Tag'
+const MEMBER = 'Mitglied'
+const SOURCE = 'Kontakt generiert durch'
+const IBAN = 'IBAN'
+const BIC = 'BIC'
+const ACCOUNT_DATA_OK = 'Kontodaten gecheckt?'
+
+module.exports = {
+   ID,
+   GENDER,
+   TITLE,
+   FIRST,
+   LAST,
+   ORGA,
+   FUNCTION,
+   OPT_OUT,
+   EMAIL_WORK,
+   EMAIL_HOME,
+   DONATE_PLATTFORM,
+   PHONE_WORK,
+   MOBILE_HOME,
+   PHONE_HOME,
+   POSTCODE,
+   CITY,
+   STREET,
+   COUNTRY,
+   BIRTH,
+   GROUP,
+   TAG,
+   MEMBER,
+   SOURCE,
+   IBAN,
+   BIC,
+   ACCOUNT_DATA_OK,
+}
