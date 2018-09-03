25bd2fc74223df294619db5455ad6dd70446a03d
arotter <andreas.rotter.kontakt@gmail.com>
2018-09-03 20:02:34 +0200

    remove store

--- src/client.js
+++ src/client.js
@@ -1,13 +1,11 @@
-import UIStore from './stores/ui-store'
 import CiviCrmImporter from './components/root'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const uiStore = new UIStore()
 const rootElement = document.getElementById('civicrm-importer-app')
 
 ReactDOM.render(
-  <CiviCrmImporter store={uiStore} />,
+  <CiviCrmImporter />,
   rootElement
 )
 
