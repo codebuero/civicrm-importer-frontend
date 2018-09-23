a5e89367bc392b0d41f1c55e25ccbcc9829af718
arotter <andreas.rotter.kontakt@gmail.com>
2018-09-23 19:00:36 +0200

    add version in header

--- src/client.js
+++ src/client.js
@@ -2,10 +2,12 @@ import CiviCrmImporter from './components/root'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const VERSION = "0.0.2"
+
 const rootElement = document.getElementById('civicrm-importer-app')
 
 ReactDOM.render(
-  <CiviCrmImporter />,
+  <CiviCrmImporter version={VERSION} />,
   rootElement
 )
 
