4cf46913120b7041ce374a53d94fda68bcdce035
arotter <andreas.rotter.kontakt@gmail.com>
2018-09-23 19:17:29 +0200

    remove legacy file

--- bin/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-var path = require('path');
-var fs = require('fs');
-
-var babelrc = JSON.parse(fs.readFileSync('./.babelrc'));
-require('babel-register')(babelrc);
-
-require('../src/app');
