67cd7cdf537baa38e4d26379add4277c796c3265
arotter <andreas.rotter.kontakt@gmail.com>
2018-09-27 10:49:38 +0200

    replace disabled button with render status button

--- src/components/import.jsx
+++ src/components/import.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React from 'react'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 
 export default class Import extends React.Component {
   static propTypes = {
@@ -8,31 +9,36 @@ export default class Import extends React.Component {
     importing: PropTypes.bool.isRequired,
     importRuns: PropTypes.number.isRequired,
   }
+
   constructor(props) {
     super(props)
 
     this.displayName = 'Import';
-    
-    this.startImport = this.startImport.bind(this);
 
     this.state = {
       loading: false,
       lastRunCount: props.importRuns
     }
   }
-  startImport() {
 
-  }
   render() {
+    const btnClasses = classnames('button','is-fullwidth','is-info', { 'is-loading': this.props.importing})
+
     return (
       <section className="import content">
         {this.state.lastRunCount === this.props.importRuns && (
           <div>
             <progress className="progress is-success" value={this.props.progress} max="100">0</progress>
-            <button disabled={this.props.importing} className="btn" onClick={this.props.onStartImport}>Begin Import</button>
+            <button className={btnClasses} onClick={this.props.onStartImport}>Begin Import</button>
           </div>
         )}
-        {this.state.lastRunCount < this.props.importRuns && (<button className="btn" onClick={this.props.onResetImport}>Reset Importer</button>)}
+        {this.state.lastRunCount < this.props.importRuns && (
+          <button 
+            className="button is-success is-fullwidth" 
+            onClick={this.props.onResetImport}
+          >
+            Reset Importer
+          </button>)}
       </section>
     );
   }
